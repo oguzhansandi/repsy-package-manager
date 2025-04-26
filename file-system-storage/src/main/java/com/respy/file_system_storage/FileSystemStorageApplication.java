02f7297a1890a4a85b588218d83105920c91663a
oguzhansandi <oguzhansandi@gmail.com>
2025-04-27 00:00:53 +0300

    initial commit: added folders and setup modules

---
+++ file-system-storage/src/main/java/com/respy/file_system_storage/FileSystemStorageApplication.java
@@ -0,0 +1,13 @@
+package com.respy.file_system_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSystemStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSystemStorageApplication.class, args);
+	}
+
+}
---
+++ respy-package/src/test/java/com/respy/respy_package_manager/RespyPackageManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.respy.respy_package_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespyPackageManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
